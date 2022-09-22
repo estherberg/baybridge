6a6beb2ee93a718d479ec8d2f6c12113b6288cd6
Esther <84743798+estherberg@users.noreply.github.com>
2022-09-22 22:16:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ import {
 } from "mdb-react-ui-kit";
 import React, { useEffect, useState } from "react";
 import data from "./data.json";
+// const fs = require('fs');
 
 export default function App() {
-
   const [products, setProducts] = useState(data);
   console.log("products", products);
 
@@ -50,9 +50,10 @@ export default function App() {
     [products]
   );
 
+  //Test fuction fs to write on data.json
   // const storeData = (data, path) => {
   //     try {
-  //         fs.writeFileSync(path, JSON.stringify(data))
+  //         fs.writeFile(path, JSON.stringify(data))
   //     } catch (err) {
   //         console.error(err)
   //     }
@@ -77,20 +78,40 @@ export default function App() {
                     <span>$53.98</span>
                   </MDBListGroupItem> */}
 
-                  <MDBListGroupItem className="justify-content-between align-items-center" style={{ minWidth: '22rem' }} light small  >
-
-                  {products.map((item) => (<div className={""}> <span className={"item_title"}> {item.product }:</span>{item.priceForAll} $ </div>))}
-                   
-                    
+                  <MDBListGroupItem
+                    className="justify-content-between align-items-center"
+                    style={{ minWidth: "22rem" }}
+                    light
+                    small
+                  >
+                    {products.map((item) => (
+                      <div className={"d-flex justify-content-between mb-2"}>
+                        {" "}
+                        <span className={"item_title"}>
+                          {" "}
+                          {item.product}:
+                        </span>{" "}
+                        {item.priceForAll} ${" "}
+                      </div>
+                    ))}
                   </MDBListGroupItem>
 
                   <MDBListGroupItem className="d-flex  justify-content-between align-items-center px-10">
-                  Shipping Value: 
-                    {products.length<4 && <div> <span className={"item_title"}></span>15$</div>}
-                    {products.length>=4 && <div> <span className={"item_title"}></span>30$</div>}
-                    
+                    Shipping Value:
+                    {products.length < 4 && (
+                      <div>
+                        {" "}
+                        <span className={"item_title"}></span>15$
+                      </div>
+                    )}
+                    {products.length >= 4 && (
+                      <div>
+                        {" "}
+                        <span className={"item_title"}></span>30$
+                      </div>
+                    )}
                   </MDBListGroupItem>
-                  
+
                   <MDBListGroupItem className="d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                     <div>
                       <strong>Total amount</strong>
@@ -99,21 +120,33 @@ export default function App() {
                       </strong>
                     </div>
                     <span>
-                      <strong> <div> <span className={"item_title"}></span>{sum} $</div></strong>
+                      <strong>
+                        {" "}
+                        <div>
+                          {" "}
+                          <span className={"item_title"}></span>
+                          {sum} $
+                        </div>
+                      </strong>
                     </span>
                   </MDBListGroupItem>
                 </MDBListGroup>
 
-                <MDBBtn block size="lg"  >
+                <MDBBtn
+                  block
+                  size="lg"
+                  // //Test fuction fs to write on data.json
+                  //       onClick={()=>{
+                  //      storeData(JSON.stringify(products), './data.json')
+                  //  }}
+                >
                   Save
-                 
                 </MDBBtn>
-               
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
 
-           {/* Shopping cart */}
+          {/* Shopping cart */}
 
           <MDBCol md="8">
             <MDBCard className="mb-4">
@@ -123,109 +156,117 @@ export default function App() {
                 </MDBTypography>
               </MDBCardHeader>
               <MDBCardBody>
-
                 {/* Product  */}
-                {products.map((item) => (<div className={'MDBRow'}>
-                <MDBRow>
+                {products.map((item) => (
+                  <div className={"MDBRow"}>
+                    <MDBRow className="d-flex justify-content-space-between align-items-center">
+                      <MDBCol lg="3" md="12" className="mb-4 mb-lg-0">
+                        <MDBRipple
+                          rippleTag="div"
+                          rippleColor="light"
+                          className="bg-image rounded hover-zoom hover-overlay"
+                        >
+                          <img src={item.Link} className="w-100" alt="" />
 
-                  <MDBCol lg="3" md="12" className="mb-4 mb-lg-0">
-                    <MDBRipple
-                      rippleTag="div"
-                      rippleColor="light"
-                      className="bg-image rounded hover-zoom hover-overlay"
-                    >
-                      <img
-                        src={item.Link}
-                        className="w-100"
-                        alt=""
-                      />
+                          <a href="#!">
+                            <div
+                              className="mask"
+                              style={{
+                                backgroundColor: "rgba(251, 251, 251, 0.2)",
+                              }}
+                            ></div>
+                          </a>
+                        </MDBRipple>
+                      </MDBCol>
 
-                      <a href="#!">
-                        <div
-                          className="mask"
-                          style={{
-                            backgroundColor: "rgba(251, 251, 251, 0.2)",
+                      <MDBCol lg="5" md="6" className=" mb-4 mb-lg-0">
+                        <p>
+                          <strong>
+                            <div className={"item"}>
+                              <span className={"item_title"}>Produit:</span>{" "}
+                              {item.product}{" "}
+                            </div>
+                          </strong>
+                        </p>
+
+                        <MDBBtn
+                          onClick={() => {
+                            setProducts((ps) =>
+                              ps.filter(
+                                (item2) => item2.product !== item.product
+                              )
+                            );
                           }}
-                        ></div>
-                      </a>
-                    </MDBRipple>
-                  </MDBCol>
-                  
-                   
-                    <MDBCol lg="5" md="6" className=" mb-4 mb-lg-0">
-                    
-                    <p>                   
-                      <strong><div className={"item"}><span className={"item_title"}>Produit:</span> {item.product} </div></strong>
-                    </p>
+                          wrapperProps={{ size: "sm" }}
+                          wrapperClass="me-1 mb-2"
+                          title="Remove item"
+                        >
+                          <MDBIcon fas icon="trash" color="danger" />
+                        </MDBBtn>
+                      </MDBCol>
 
-                    <MDBBtn onClick={()=>{
-                        setProducts((ps) => ps.filter((item2) => (item2.product !== item.product)));
-                    }}
-                      wrapperProps={{ size: "sm" }}
-                      wrapperClass="me-1 mb-2"
-                      title="Remove item"
-                    >
-                      <MDBIcon fas icon="trash" color="danger" />
-                    </MDBBtn>
-                  </MDBCol>
-                
-                  
+                      <MDBCol lg="4" md="6" className="mb-4 mb-lg-0">
+                        <div
+                          className="d-flex mb-4"
+                          style={{ maxWidth: "300px" }}
+                        >
+                          <MDBBtn
+                            className="px-3 me-2"
+                            onClick={() => {
+                              setProducts((ps) =>
+                                ps.map((item2) => {
+                                  if (item2.product === item.product) {
+                                    return {
+                                      ...item2,
+                                      quantity: item2.quantity - 1,
+                                    };
+                                  } else {
+                                    return item2;
+                                  }
+                                })
+                              );
+                            }}
+                          >
+                            <MDBIcon fas icon="minus" />
+                          </MDBBtn>
 
-                  <MDBCol lg="4" md="6" className="mb-4 mb-lg-0">
-                    <div className="d-flex mb-4" style={{ maxWidth: "300px" }}>
-                      <MDBBtn className="px-3 me-2"
-                      onClick={() => {
-                        setProducts((ps) => ps.map((item2) => {
-                            if (item2.product === item.product) {
-                                return {
-                                    ...item2,
-                                    quantity: item2.quantity - 1,
-                                };
-                            } else {
-                                return item2;
-                            }
-                        }));
-                    }}>
-                        <MDBIcon fas icon="minus" />
-                      </MDBBtn>
+                          <MDBInput
+                            value={item.quantity}
+                            type="number"
+                            min={0}
+                            label="Quantity"
+                          />
 
-                      <MDBInput
-                        value={item.quantity}
-                        min={0}
-                        type="number"
-                        label="Quantity"
-                      />
+                          <MDBBtn
+                            className="px-3 ms-2"
+                            onClick={() => {
+                              setProducts((ps) =>
+                                ps.map((item2) => {
+                                  if (item2.product === item.product) {
+                                    return {
+                                      ...item2,
+                                      quantity: item2.quantity + 1,
+                                    };
+                                  } else {
+                                    return item2;
+                                  }
+                                })
+                              );
+                            }}
+                            id="quantity"
+                          >
+                            <MDBIcon fas icon="plus" />
+                          </MDBBtn>
+                        </div>
 
-
-                      <MDBBtn className="px-3 ms-2" 
-                      onClick={() => {
-                        setProducts((ps) => ps.map((item2) => {
-                            if (item2.product === item.product) {
-                                return {
-                                    ...item2,
-                                    quantity: item2.quantity + 1,
-                                };
-                            } else {
-                                return item2;
-                            }
-                        }));
-                    }}
-                    
-                    id="quantity"
-                    >
-                        <MDBIcon fas icon="plus" />
-                      </MDBBtn>
-                    </div>
-
-                    <p className="text-start text-md-center">
-                      <strong>{item.price}$</strong>
-                    </p>
-                  </MDBCol>
-                </MDBRow>
-                </div>))}
+                        <p className="text-start text-md-center">
+                          <strong>{item.price}$</strong>
+                        </p>
+                      </MDBCol>
+                    </MDBRow>
+                  </div>
+                ))}
                 <hr className="my-4" />
-
-               
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
